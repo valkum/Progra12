a909408655f5fbd9b49de2eb5acd2a151455c7b0
valkum <rudi.floren@googlemail.com>
2012-11-08 15:28:59 +0100

    ...

--- uebung4/Muell/Band.java
+++ uebung4/Muell/Band.java
@@ -1,3 +1,24 @@
 public class Band {
   public Muell[] inhalt;
+
+
+  public static String arrayToString(Muell[] m) {
+  	String string = "";
+  	for(int i= 0; i < m.length; i++) {
+  		if (m[i] != null) string += m[i].toString();
+  	}
+  	return string;
+  }
+
+  public String toString() {
+  	return this.arrayToString(this.inhalt);
+  }
+  
+  public void drauf(Muell m){
+    for(int i = 0; i < this.inhalt.length; i++){
+      if(this.inhalt[i] == null){
+        this.inhalt[i] = m;
+      }
+    }
+  }
 }
---
+++ uebung4/Muell/Sorte.java
@@ -0,0 +1,4 @@
+public enum Sorte {
+  Rest, Metall, Plastik;
+}
+
